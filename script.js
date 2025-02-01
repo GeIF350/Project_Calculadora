cecb19df463e4e66c291c9d8ad5e41c52896066f
GeIF350 <d.geovana@estudante.ifmt.edu.br>
2025-01-31 20:29:43 -0400

    apenas excluí os 'ifs'

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
 let resultado=0;
 const numero1 = document.querySelector("#numero1");
 const numero2 = document.querySelector("#numero2");
-if(onclick==buttonSoma){
-    function buttonSoma(){
-        let soma;
-        soma = parseFloat(numero1.value)+parseFloat(numero2.value);
-        resultado = soma;
-        console.log(soma);
-        document.getElementById("result").innerHTML=""+resultado;
-        document.getElementById("resultado").innerHTML=""+resultado;
-    };
-}
-if(onclick==buttonMinus){
-    function buttonMinus(){
-        let subtracao;
-        subtracao = parseFloat(numero1.value)-parseFloat(numero2.value);
-        resultado = subtracao;
-        console.log(subtracao);
-        document.getElementById("result").innerHTML=""+resultado;
-        document.getElementById("resultado").innerHTML=""+resultado;
-    };
-}
-if(onclick==buttonMult){
-    function buttonMult(){
-        let mult;
-        mult = parseFloat(numero1.value)*parseFloat(numero2.value);
-        resultado = mult;
-        console.log(mult);
-        document.getElementById("result").innerHTML=""+resultado;
-        document.getElementById("resultado").innerHTML=""+resultado;
-    };
-}
-if(onclick==buttonDiv){
-    function buttonDiv(){
-        let div;
-        div = parseFloat(numero1.value)/parseFloat(numero2.value);
-        resultado = div;
-        console.log(div);
-        document.getElementById("result").innerHTML=""+resultado;
-        document.getElementById("resultado").innerHTML=""+resultado;
-    };
-}
+function buttonSoma(){
+    let soma;
+    soma = parseFloat(numero1.value)+parseFloat(numero2.value);
+    resultado = soma;
+    document.getElementById("result").innerHTML=""+resultado;
+    document.getElementById("resultado").innerHTML=""+resultado;
+};
+    
+function buttonMinus(){
+    let subtracao;
+    subtracao = parseFloat(numero1.value)-parseFloat(numero2.value);
+    resultado = subtracao;
+    document.getElementById("result").innerHTML=""+resultado;
+    document.getElementById("resultado").innerHTML=""+resultado;
+};
+    
+function buttonMult(){
+    let mult;
+    mult = parseFloat(numero1.value)*parseFloat(numero2.value);
+    resultado = mult;
+    document.getElementById("result").innerHTML=""+resultado;
+    document.getElementById("resultado").innerHTML=""+resultado;
+};
+    
+function buttonDiv(){
+    let div;
+    div = parseFloat(numero1.value)/parseFloat(numero2.value);
+    resultado = div;
+    document.getElementById("result").innerHTML=""+resultado;
+    document.getElementById("resultado").innerHTML=""+resultado;
+};
